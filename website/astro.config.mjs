0fd8a4a26c63817e6af2c944ae603cb19a9db4ed
Deep Sarda <deep.p.sarda@gmail.com>
2026-05-28 21:08:03 +0530

    fix(website): Fix to match github pages url

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -7,6 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://deepsarda.github.io',
+  base: '/Nox',
   integrations: [
     starlight({
       title: 'Nox',
